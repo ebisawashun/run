d5b4816a4f67a170046523b22f8ab940721d9db8
ebisawashun <shun171219@icloud.com>
2024-06-24 19:13:37 +0900

    ジャンプ

--- Assets/2D Pixel Art Icon/Pixel Adventure 1/Assets/run.cs
+++ Assets/2D Pixel Art Icon/Pixel Adventure 1/Assets/run.cs
@@ -7,9 +7,17 @@ public class run : MonoBehaviour
     [SerializeField] private float movespeed = 8f,jumpForce = 7f;
     private Rigidbody2D rb;
 
+    private Transform groundCheckpoint;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+    
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        groundCheckpoint = transform.GetChild(0).transform;
     }
 
     // Start is called before the first frame update
@@ -21,7 +29,7 @@ public class run : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerJump();
     }
 
     private void FixedUpdate()
@@ -32,5 +40,20 @@ public class run : MonoBehaviour
     private void MovePlayer()
     {
         rb.velocity = new Vector2(movespeed,rb.velocity.y); 
+    }
+    private void PlayerJump()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (IsGrounded())
+            {
+                rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            }
+
+        }
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheckpoint.position, 0.1f, groundLayer);
     }
 }
